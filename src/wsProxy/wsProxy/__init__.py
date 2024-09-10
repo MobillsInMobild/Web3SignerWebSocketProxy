3cf44642bfa48ed29b9fd0c28dfa31557a742382
MobillsInMobild <vgeekv@163.com>
2024-09-10 11:17:51 +0800

    build: updata wsProxy Package dir structure

---
+++ src/wsProxy/wsProxy/__init__.py
@@ -0,0 +1,2 @@
+from .wsServer import WebSocketServer  # noqa: F401
+from .wsClient import WebSocketClient  # noqa: F401
--- src/wsProxy/wsProxy/wsClient.py
+++ src/wsProxy/wsProxy/wsClient.py
@@ -9,7 +9,11 @@ class WebSocketClient:
     async def send_transaction(self, transaction):
         async with websockets.connect(self.uri) as websocket:
             print(f"Sending: {transaction}")
-            await websocket.send(json.dumps(transaction))
+            message = {
+                'method': 'eth_sendTransaction',
+                'params': [transaction],
+            }
+            await websocket.send(json.dumps(message))
             response = await websocket.recv()
             print(f"Response: {response}")
             return response
